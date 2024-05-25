cf7ad683ef6fb912e7db9ab772f4407433adeb45
Vishnuraj24 <117718708+Vishnuraj24@users.noreply.github.com>
2024-05-25 13:09:15 +0530

    Time: 473 ms (5.02%), Space: 239.9 MB (5.01%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int s;
+    vector<vector<int>> output;
+    void solve(vector<int>&nums,vector<int>temp,int index){
+        
+        //base case
+        if(temp.size() == s){
+            output.push_back(temp);
+        }
+        
+        //main logic
+        
+        for(int i = index; i < nums.size(); i++){
+            
+            temp.push_back(nums[i]); //do something
+            solve(nums,temp,i+1); //explore
+            
+            temp.pop_back();   //revert to step 1
+            //solve(nums,temp,i+1); // further explore
+        }
+        
+    }
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>nums;
+        vector<int>temp;
+        s = k;
+        int start = 0;
+        for(int i = 1 ; i <= n; i++){
+            nums.push_back(i);
+        }
+        
+        solve(nums,temp,start);
+        return output;
+        
+    }
+};
